132c4653fe5b77b4c9141872797cf65064f2b36d
Silica <1484702+Silica@users.noreply.github.com>
2012-03-02 11:34:27 +0900

    memory.h コード整理

--- memory.h
+++ memory.h
@@ -1,41 +1,14 @@
 
 // Variable::operator new専用(8バイト固定)メモリマネージャ
+// Variableはメンバが0にならない、という仕様を利用しているので
+// 他に応用するには使用中・未使用に関する判定の修正が必要
 
 class MemoryManager
 {
-	const static int poolsize = 256;
 public:
-	static int *Next()
-	{
-		int *ptr = pool();
-		for (int i = current(); i < poolsize; i += 2)
-		{
-			if (!ptr[i])
-			{
-				current() = i+2;
-				count() += 1;;
-				return ptr + i;
-			}
-		}
-		for (int i = 0; i < current(); i += 2)
-		{
-			if (!ptr[i])
-			{
-				current() = i+2;
-				count() += 1;;
-				return ptr + i;
-			}
-		}
-		std::printf("throw,%d,%d", count(), current());
-		throw std::bad_alloc();
-	}
-	static void Release(void *ptr)
-	{
-		int *p = pool();
-		current() = (int*)ptr - p;
-		count()--;
-		p[current()] = 0;
-	}
+	const static int poolsize = 256;
+	static int *Next()				{return MemoryPool().nextptr();}
+	static void Release(void *ptr)	{MemoryPool().release((int*)ptr);}
 private:
 	class mpool
 	{
@@ -46,8 +19,10 @@ private:
 		NextとReleaseが管理するのではなく
 		こいつのメンバに値を渡して再帰的に処理して貰う
 		*/
+		int current;
+		int count;
 	public:
-		mpool(size_t t)	{ptr = new int[t];std::memset(ptr, 0, t*sizeof(int));}
+		mpool(size_t t)	{current = count = 0;ptr = new int[t];std::memset(ptr, 0, t*sizeof(int));}
 		~mpool()		{
 			// これでメモリリークを回避出来る
 			// が、開放されないまま終了するのを回避出来るだけで
@@ -64,27 +39,40 @@ private:
 			delete []ptr;
 //			std::printf("end,%d,%d", count(), current());
 		}
-		int *get()		{return ptr;}
+		int *nextptr()
+		{
+			for (int i = current; i < poolsize; i += 2)
+			{
+				if (!ptr[i])
+				{
+					current = i+2;
+					count += 1;
+					return ptr + i;
+				}
+			}
+			for (int i = 0; i < current; i += 2)
+			{
+				if (!ptr[i])
+				{
+					current = i+2;
+					count += 1;
+					return ptr + i;
+				}
+			}
+			std::printf("throw,%d,%d\n", count, current);
+			throw std::bad_alloc();
+		}
+		void release(int *p)
+		{
+			current = p - ptr;
+			count--;
+			ptr[current] = 0;
+		}
 	};
-	static int *pool()
-	{
-		return MemoryPool().get();
-	}
 	static mpool &MemoryPool()
 	{
 		static mpool p(poolsize);
 		return p;
 	}
-	static int &current()
-	{
-		static int c = 0;
-		return c;
-	}
-	static int &count()
-	{
-		static int c = 0;
-		return c;
-	}
-	friend class mpool;
 };
 
