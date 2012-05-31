4e280548f5926b228f49b8892cbcb309f80ed680
Silica <1484702+Silica@users.noreply.github.com>
2012-05-31 16:05:15 +0900

    constメソッドをバインダで登録出来る様にした

--- PSL/binder.h
+++ PSL/binder.h
@@ -62,12 +62,19 @@ static vBase *BCFunction(R(*f)(A0,A1,A2,A3,A4,A5)){return new vBCFunctionR6<R,A0
 	void prepare(Environment &env, Variable *v){variable a = env.pop();C*i=static_cast<C*>(this_v->index(0)->toPointer());(i->*x)g;env.push(a);}\
 	rsv call(Environment &env, variable &a, Variable *v){C*i=static_cast<C*>(this_v->index(0)->toPointer());(i->*x)g;return a;}};
 template<class C>MV(0,(),())
+template<class C>MV(C0,()const,())
 template<class C, class A0>MV(1,(A0),(a[0]))
+template<class C, class A0>MV(C1,(A0)const,(a[0]))
 template<class C, class A0, class A1>MV(2,(A0,A1),(a[0],a[1]))
+template<class C, class A0, class A1>MV(C2,(A0,A1)const,(a[0],a[1]))
 template<class C, class A0, class A1, class A2>MV(3,(A0,A1,A2),(a[0],a[1],a[2]))
+template<class C, class A0, class A1, class A2>MV(C3,(A0,A1,A2)const,(a[0],a[1],a[2]))
 template<class C, class A0, class A1, class A2, class A3>MV(4,(A0,A1,A2,A3),(a[0],a[1],a[2],a[3]))
+template<class C, class A0, class A1, class A2, class A3>MV(C4,(A0,A1,A2,A3)const,(a[0],a[1],a[2],a[3]))
 template<class C, class A0, class A1, class A2, class A3, class A4>MV(5,(A0,A1,A2,A3,A4),(a[0],a[1],a[2],a[3],a[4]))
+template<class C, class A0, class A1, class A2, class A3, class A4>MV(C5,(A0,A1,A2,A3,A4)const,(a[0],a[1],a[2],a[3],a[4]))
 template<class C, class A0, class A1, class A2, class A3, class A4, class A5>MV(6,(A0,A1,A2,A3,A4,A5),(a[0],a[1],a[2],a[3],a[4],a[5]))
+template<class C, class A0, class A1, class A2, class A3, class A4, class A5>MV(C6,(A0,A1,A2,A3,A4,A5)const,(a[0],a[1],a[2],a[3],a[4],a[5]))
 #undef MV
 #define MR(n,t,g) class vCCMethodR##n : public vBase{R(C::*x)t;Variable *this_v;public:\
 	PSL_MEMORY_MANAGER(vCCMethodR##n)\
@@ -77,38 +84,73 @@ template<class C, class A0, class A1, class A2, class A3, class A4, class A5>MV(
 	void prepare(Environment &env, Variable *v){variable a = env.pop();C*i=static_cast<C*>(this_v->index(0)->toPointer());variable r = (i->*x)g;env.push(r);}\
 	rsv call(Environment &env, variable &a, Variable *v){C*i=static_cast<C*>(this_v->index(0)->toPointer());variable r = (i->*x)g;return r;}};
 template<class C, class R>MR(0,(),())
+template<class C, class R>MR(C0,()const,())
 template<class C, class R, class A0>MR(1,(A0),(a[0]))
+template<class C, class R, class A0>MR(C1,(A0)const,(a[0]))
 template<class C, class R, class A0, class A1>MR(2,(A0,A1),(a[0],a[1]))
+template<class C, class R, class A0, class A1>MR(C2,(A0,A1)const,(a[0],a[1]))
 template<class C, class R, class A0, class A1, class A2>MR(3,(A0,A1,A2),(a[0],a[1],a[2]))
+template<class C, class R, class A0, class A1, class A2>MR(C3,(A0,A1,A2)const,(a[0],a[1],a[2]))
 template<class C, class R, class A0, class A1, class A2, class A3>MR(4,(A0,A1,A2,A3),(a[0],a[1],a[2],a[3]))
+template<class C, class R, class A0, class A1, class A2, class A3>MR(C4,(A0,A1,A2,A3)const,(a[0],a[1],a[2],a[3]))
 template<class C, class R, class A0, class A1, class A2, class A3, class A4>MR(5,(A0,A1,A2,A3,A4),(a[0],a[1],a[2],a[3],a[4]))
+template<class C, class R, class A0, class A1, class A2, class A3, class A4>MR(C5,(A0,A1,A2,A3,A4)const,(a[0],a[1],a[2],a[3],a[4]))
 template<class C, class R, class A0, class A1, class A2, class A3, class A4, class A5>MR(6,(A0,A1,A2,A3,A4,A5),(a[0],a[1],a[2],a[3],a[4],a[5]))
+template<class C, class R, class A0, class A1, class A2, class A3, class A4, class A5>MR(C6,(A0,A1,A2,A3,A4,A5)const,(a[0],a[1],a[2],a[3],a[4],a[5]))
 #undef MR
 template<class C>
 static vBase *CCMethod(void(C::*m)()){return new vCCMethodV0<C>(m);}
+template<class C>
+static vBase *CCMethod(void(C::*m)()const){return new vCCMethodVC0<C>(m);}
 template<class C, class A0>
 static vBase *CCMethod(void(C::*m)(A0)){return new vCCMethodV1<C,A0>(m);}
+template<class C, class A0>
+static vBase *CCMethod(void(C::*m)(A0)const){return new vCCMethodVC1<C,A0>(m);}
 template<class C, class A0, class A1>
 static vBase *CCMethod(void(C::*m)(A0,A1)){return new vCCMethodV2<C,A0,A1>(m);}
+template<class C, class A0, class A1>
+static vBase *CCMethod(void(C::*m)(A0,A1)const){return new vCCMethodVC2<C,A0,A1>(m);}
 template<class C, class A0, class A1, class A2>
 static vBase *CCMethod(void(C::*m)(A0,A1,A2)){return new vCCMethodV3<C,A0,A1,A2>(m);}
+template<class C, class A0, class A1, class A2>
+static vBase *CCMethod(void(C::*m)(A0,A1,A2)const){return new vCCMethodVC3<C,A0,A1,A2>(m);}
 template<class C, class A0, class A1, class A2, class A3>
 static vBase *CCMethod(void(C::*m)(A0,A1,A2,A3)){return new vCCMethodV4<C,A0,A1,A2,A3>(m);}
+template<class C, class A0, class A1, class A2, class A3>
+static vBase *CCMethod(void(C::*m)(A0,A1,A2,A3)const){return new vCCMethodVC4<C,A0,A1,A2,A3>(m);}
 template<class C, class A0, class A1, class A2, class A3, class A4>
 static vBase *CCMethod(void(C::*m)(A0,A1,A2,A3,A4)){return new vCCMethodV5<C,A0,A1,A2,A3,A4>(m);}
+template<class C, class A0, class A1, class A2, class A3, class A4>
+static vBase *CCMethod(void(C::*m)(A0,A1,A2,A3,A4)const){return new vCCMethodVC5<C,A0,A1,A2,A3,A4>(m);}
 template<class C, class A0, class A1, class A2, class A3, class A4, class A5>
 static vBase *CCMethod(void(C::*m)(A0,A1,A2,A3,A4,A5)){return new vCCMethodV6<C,A0,A1,A2,A3,A4,A5>(m);}
+template<class C, class A0, class A1, class A2, class A3, class A4, class A5>
+static vBase *CCMethod(void(C::*m)(A0,A1,A2,A3,A4,A5)const){return new vCCMethodVC6<C,A0,A1,A2,A3,A4,A5>(m);}
 template<class C, class R>
 static vBase *CCMethod(R(C::*m)()){return new vCCMethodR0<C,R>(m);}
+template<class C, class R>
+static vBase *CCMethod(R(C::*m)()const){return new vCCMethodRC0<C,R>(m);}
 template<class C, class R, class A0>
 static vBase *CCMethod(R(C::*m)(A0)){return new vCCMethodR1<C,R,A0>(m);}
+template<class C, class R, class A0>
+static vBase *CCMethod(R(C::*m)(A0)const){return new vCCMethodRC1<C,R,A0>(m);}
 template<class C, class R, class A0, class A1>
 static vBase *CCMethod(R(C::*m)(A0,A1)){return new vCCMethodR2<C,R,A0,A1>(m);}
+template<class C, class R, class A0, class A1>
+static vBase *CCMethod(R(C::*m)(A0,A1)const){return new vCCMethodRC2<C,R,A0,A1>(m);}
 template<class C, class R, class A0, class A1, class A2>
 static vBase *CCMethod(R(C::*m)(A0,A1,A2)){return new vCCMethodR3<C,R,A0,A1,A2>(m);}
+template<class C, class R, class A0, class A1, class A2>
+static vBase *CCMethod(R(C::*m)(A0,A1,A2)const){return new vCCMethodRC3<C,R,A0,A1,A2>(m);}
 template<class C, class R, class A0, class A1, class A2, class A3>
 static vBase *CCMethod(R(C::*m)(A0,A1,A2,A3)){return new vCCMethodR4<C,R,A0,A1,A2,A3>(m);}
+template<class C, class R, class A0, class A1, class A2, class A3>
+static vBase *CCMethod(R(C::*m)(A0,A1,A2,A3)const){return new vCCMethodRC4<C,R,A0,A1,A2,A3>(m);}
 template<class C, class R, class A0, class A1, class A2, class A3, class A4>
 static vBase *CCMethod(R(C::*m)(A0,A1,A2,A3,A4)){return new vCCMethodR5<C,R,A0,A1,A2,A3,A4>(m);}
+template<class C, class R, class A0, class A1, class A2, class A3, class A4>
+static vBase *CCMethod(R(C::*m)(A0,A1,A2,A3,A4)const){return new vCCMethodRC5<C,R,A0,A1,A2,A3,A4>(m);}
 template<class C, class R, class A0, class A1, class A2, class A3, class A4, class A5>
 static vBase *CCMethod(R(C::*m)(A0,A1,A2,A3,A4,A5)){return new vCCMethodR6<C,R,A0,A1,A2,A3,A4,A5>(m);}
+template<class C, class R, class A0, class A1, class A2, class A3, class A4, class A5>
+static vBase *CCMethod(R(C::*m)(A0,A1,A2,A3,A4,A5)const){return new vCCMethodRC6<C,R,A0,A1,A2,A3,A4,A5>(m);}
